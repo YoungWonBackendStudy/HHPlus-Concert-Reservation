422960ce9f6116051d3e723d9289be88956c392a
Youngwon <71220342+yw7148@users.noreply.github.com>
2024-07-12 14:17:53 +0900

    docs: Swagger 적용 (#10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//swagger-ui
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
 
 tasks.withType<Test> {
