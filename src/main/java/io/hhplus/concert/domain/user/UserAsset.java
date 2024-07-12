605d6b8eeb007844e694c306c9b4379746975c8e
yw7148 <yw7148@naver.com>
2024-07-12 04:36:57 +0000

    feat: 사용자 자산 관련 기능 개발

---
+++ src/main/java/io/hhplus/concert/domain/user/UserAsset.java
@@ -0,0 +1,21 @@
+package io.hhplus.concert.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserAsset {
+    long userId;
+    long balance;
+
+    public void charge(long amount) {
+        this.balance += amount;
+    }
+
+    public void use(long amount) {
+        if(this.balance < amount) throw new RuntimeException("잔액이 부족합니다.");
+
+        this.balance -= amount;
+    }
+}
