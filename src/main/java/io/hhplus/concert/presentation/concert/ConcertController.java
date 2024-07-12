a481d4a4c0dc2f366ecb0aa40746c74a2c2dba93
yw7148 <yw7148@naver.com>
2024-07-12 05:11:24 +0000

    docs: Swagger 적용

--- src/main/java/io/hhplus/concert/presentation/concert/ConcertController.java
+++ src/main/java/io/hhplus/concert/presentation/concert/ConcertController.java
@@ -9,9 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 import io.hhplus.concert.presentation.concert.dto.ConcertDto;
 import io.hhplus.concert.presentation.concert.dto.ConcertScheduleDto;
 import io.hhplus.concert.presentation.concert.dto.ConcertScheduleSeatDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "콘서트 정보")
 @RestController
 public class ConcertController {
+
+    @Operation(summary = "콘서트 목록 조회 API")
+    @Parameters(value = {
+        @Parameter(name = "token", required = true, description = "ACTIVE상태의 토큰")
+    })
     @GetMapping("concerts")
     public List<ConcertDto.Response> getConcerts(
        ConcertDto.Request concertRequest 
@@ -19,6 +29,11 @@ public class ConcertController {
         return List.of(new ConcertDto.Response(0l, "아이유 콘서트", "2024.07.05 ~ 2024.07.06 아이유 콘서트"));
     }
 
+    @Operation(summary = "콘서트 스케줄 조회 API")
+    @Parameters(value = {
+        @Parameter(name = "token", required = true, description = "ACTIVE상태의 토큰"),
+        @Parameter(name = "concertScheduleId", required = true, description = "조회할 콘서트의 ID")
+    })
     @GetMapping("concerts/schedules")
     public List<ConcertScheduleDto.Response> concertSchedules(
         ConcertScheduleDto.Request concertScheduleRequest
@@ -26,6 +41,11 @@ public class ConcertController {
         return List.of(new ConcertScheduleDto.Response(0l, new Date(), "서울 잠실"));
     }
 
+    @Operation(summary = "콘서트 좌석 조회 API")
+    @Parameters(value = {
+        @Parameter(name = "token", required = true, description = "ACTIVE상태의 토큰"),
+        @Parameter(name = "concertScheduleId", required = true, description = "조회할 콘서트 스케줄의 ID")
+    })
     @GetMapping("concerts/schedules/seats")
     public List<ConcertScheduleSeatDto.Response> concertScheduleSeats(
         ConcertScheduleSeatDto.Request concertSeatRequest
--- src/main/java/io/hhplus/concert/presentation/payment/PaymentController.java
+++ src/main/java/io/hhplus/concert/presentation/payment/PaymentController.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.hhplus.concert.presentation.payment.dto.PaymentDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "결제")
 @RestController
 public class PaymentController {
-    
+    @Operation(summary = "결제 API")
+    @Parameters(value = {
+        @Parameter(name = "token", required = true, description = "ACTIVE상태의 토큰"),
+        @Parameter(name = "reservationId", required = true, description = "결제할 예약 정보의 ID")
+    })
     @PostMapping("pay")
     public PaymentDto.Response placePayment(
         @RequestBody PaymentDto.Request paymentRequest
